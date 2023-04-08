300192804c3103856e6a2b151f96c1a208ff5548
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-04-08 14:55:36 +0300

    second task completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const murkup = function (array) {
+  array.map(elem => {
+  const liEl = document.createElement('li');
+  liEl.textContent = elem;
+  liEl.classList.add('item');
+  return ingredientsEl.append(liEl)
+})
+}(ingredients);
+
+// const murkup = ingredients.map(elem => {
+//   const liEl = document.createElement('li');
+//   liEl.textContent = elem;
+//   liEl.classList.add('item');
+//   return ingredientsEl.append(liEl)
+// });
+
+
+
+
+
