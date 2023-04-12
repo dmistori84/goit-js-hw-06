f67b1fac5a856fcd64389f9e8edad20b3dc7e669
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2023-04-12 12:16:38 +0300

    done 8, 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+  }
+
+    const user = {
+        email: email.value,
+        password: password.value
+    };
+
+    console.log('user:', user);
+    event.currentTarget.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,18 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() { 
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+  //span.textContent = getRandomHexColor(); ---- наче працює, але кольори не співпадають
+};
+
+
